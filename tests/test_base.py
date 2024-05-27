8cad203eed3e65464f6bb7ec10073fbfde169d17
tuna7588 <tuna7588@users.noreply.github.com>
2024-05-27 12:43:38 +0000

    ✅ Ready to clone and code.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from fp64_anhtunguyen_familystatic.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "fp64_anhtunguyen_familystatic"
